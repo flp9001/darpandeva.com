3327a45e67863f2568ae66330328bddcc801b7d5
Felipe holanda <flp9001@gmail.com>
2019-12-09 14:26:38 -0300

    Fix typo in middlewere

--- darpan/apps/core/middleware.py
+++ darpan/apps/core/middleware.py
@@ -8,14 +8,13 @@ class XForwardedForMiddleware(object):
         # the view (and later middleware) are called.
         self.__process_request(request)
         response = self.get_response(request)
-
         # Code to be executed for each request/response after
         # the view is called.
 
         return response
 
     def __process_request(self, request):
-        if HTTP_X_FORWARDED_FOR in request.META:
+        if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META["HTTP_X_PROXY_REMOTE_ADDR"] = request.META["REMOTE_ADDR"]
             parts = request.META["HTTP_X_FORWARDED_FOR"].split(",", 1)
             request.META["REMOTE_ADDR"] = parts[0]
