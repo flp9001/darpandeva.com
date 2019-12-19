9fe9144b7a93070de96d84ff79b340d178ff63e9
Felipe holanda <flp9001@gmail.com>
2019-12-19 15:32:55 -0300

    Django CMS Start

--- darpan/manage.py
+++ darpan/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'darpan.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,12 +14,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-        
-    # This allows easy placement of apps within the interior
-    # darpan directory.
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "darpan"))
-    
     execute_from_command_line(sys.argv)
 
 
